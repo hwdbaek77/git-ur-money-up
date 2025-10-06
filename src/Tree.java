b57074a272a50ec5dd2ca84a5684b5ef25d7e9f0
Chase Mayer <cmayer2@hwemail.com>
2025-10-06 00:54:53 -0700

    (GP-3.1): Successfully generating simple tree files

--- src/Tree.java
+++ src/Tree.java
@@ -4,21 +4,23 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class Tree {
     // Creates a tree file with the given lines
     // The file will be in the objects folder and its name will be the SHA-1 hash of its contents
     // Returns the SHA-1 hash of the file's contents (its name)
-    public static String addTree(String[] entries) {
+    public static String addTree(ArrayList<String> entries) {
         ArrayList<Byte> bytes = new ArrayList<>();
-        for (int i = 0; i < entries.length; i++) {
-            String entry = entries[i];
+        for (int i = 0; i < entries.size(); i++) {
+            String entry = entries.get(i);
             byte[] curBytes = entry.getBytes();
             for (byte curByte : curBytes) {
                 bytes.add(curByte);
             }
             // We don't want our file to end with an extra newline
-            if (i != entries.length - 1) {
+            if (i != entries.size() - 1) {
                 Byte newline = '\n';
                 bytes.add(newline);
             }
@@ -37,38 +39,62 @@ public class Tree {
             String workingList = "";
             BufferedReader br = new BufferedReader(new FileReader("git/index"));
             while (br.ready()) {
-                workingList += "blob " + br.readLine();
+                workingList += "blob " + br.readLine() + "\n";
             }
             br.close();
 
             HashMap<String, ArrayList<String>> trees = new HashMap<>();
 
+            int lastNumberSlashes = -1;
             while (needToMakeTrees(workingList)) {
                 int lineIndex = findMostNestedFile(workingList);
                 String[] lines = workingList.split("\n");
                 String line = lines[lineIndex];
-                String path = line.split(" ")[2];
+                int numberSlashes = line.split("/").length - 1;
+                ArrayList<String> linesArray = new ArrayList<>();
+                for (String curLine : lines) {
+                    linesArray.add(curLine);
+                }
+                if (numberSlashes != lastNumberSlashes) {
+                    for (Entry<String, ArrayList<String>> entry : trees.entrySet()) {
+                        String key = entry.getKey();
+                        ArrayList<String> value = entry.getValue();
+                        linesArray.add("tree " + addTree(value) + " " + key);
+                    }
+                    trees.clear();
+                }
+                
+                String[] components = line.split(" ");
+                String path = components[2];
+                String fileType = components[0];
+                String fileHash = components[1];
                 int lastSlash = path.lastIndexOf("/");
                 String folder = path.substring(0, lastSlash);
                 String file = path.substring(lastSlash + 1);
                 if (trees.containsKey(folder)) {
-                    trees.get(folder).add(file);
+                    trees.get(folder).add(fileType + " " + fileHash + " " + file);
                 } else {
                     trees.put(folder, new ArrayList<>());
-                    trees.get(folder).add(file);
-                    // TODO: add tree to working list
+                    trees.get(folder).add(fileType + " " + fileHash + " " + file);
                 }
 
                 workingList = "";
-                for (int i = 0; i < lines.length; i++) {
+                for (int i = 0; i < linesArray.size(); i++) {
                     if (i != lineIndex) {
-                        workingList += lines[i];
+                        workingList += linesArray.get(i);
                     }
-                    if (i != lines.length - 1) {
+                    if (i != linesArray.size() - 1) {
                         workingList += "\n";
                     }
                 }
+                lastNumberSlashes = numberSlashes;
             }
+            for (Entry<String, ArrayList<String>> entry : trees.entrySet()) {
+                String key = entry.getKey();
+                ArrayList<String> value = entry.getValue();
+                addTree(value);
+            }
+            trees.clear();
         } catch (Exception e) {
             System.out.println("Failed to create trees!");
             e.printStackTrace();
--- src/TreeTester.java
+++ src/TreeTester.java
@@ -5,23 +5,25 @@ public class TreeTester {
     public static void main(String[] args) {
         Blob.ensureObjectsDir();
 
-        File directory = new File("testDir");
+        File directory = new File("testDir/bloooob");
         directory.mkdir();
 
-        File file = new File("testDir/testFile.txt");
+        File file = new File("testDir/bloooob/testFile.txt");
+        File file2 = new File("testDir/dog.txt");
         try {
             file.createNewFile();
             String contents = "Hello";
             Files.write(file.toPath(), contents.getBytes());
+
+            file2.createNewFile();
+            String contents2 = "Byte";
+            Files.write(file2.toPath(), contents2.getBytes());
         } catch (Exception e) {
             e.printStackTrace();
         }
 
         Blob.createDirectory("testDir");
 
-        Tree.addTree(new String[]
-        {
-            "blob bhfuerhgviurehgu dog.txt"
-        });
+        Tree.createTrees();
     }
 }
