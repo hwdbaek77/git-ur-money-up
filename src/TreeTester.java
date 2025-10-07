906ac310417c67e2505913b4ca2c99b6fbe69db1
Chase Mayer <cmayer2@hwemail.com>
2025-10-06 20:04:13 -0700

    (GP-3.1): Finish generating trees, update tester and README

--- src/TreeTester.java
+++ src/TreeTester.java
@@ -5,19 +5,26 @@ public class TreeTester {
     public static void main(String[] args) {
         Blob.ensureObjectsDir();
 
-        File directory = new File("testDir/bloooob");
-        directory.mkdir();
+        File directory = new File("testDir/srx/docs");
+        directory.mkdirs();
+        File apples = new File("testDir/apples");
+        apples.mkdir();
 
-        File file = new File("testDir/bloooob/testFile.txt");
-        File file2 = new File("testDir/dog.txt");
+        File file = new File("testDir/apples/info.ini");
+        File file2 = new File("testDir/srx/docs/README.txt");
+        File file3 = new File("testDir/drive.txt");
         try {
             file.createNewFile();
-            String contents = "Hello";
+            String contents = "informative information";
             Files.write(file.toPath(), contents.getBytes());
 
             file2.createNewFile();
-            String contents2 = "Byte";
+            String contents2 = "PSYCH! Don't Read Me";
             Files.write(file2.toPath(), contents2.getBytes());
+
+            file3.createNewFile();
+            String contents3 = "drive";
+            Files.write(file3.toPath(), contents3.getBytes());
         } catch (Exception e) {
             e.printStackTrace();
         }
