5d35a230816c93ed8743ed2349041b9345876692
jreiner16 <joereiner16@gmail.com>
2025-10-12 12:06:44 -0700

    Added some file tests and set up basic stuff for demo

--- src/GitWrapper.java
+++ src/GitWrapper.java
@@ -1,7 +1,6 @@
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.time.LocalDate;
 import java.util.Stack;
 
--- src/GitWrapperTester.java
+++ src/GitWrapperTester.java
@@ -2,12 +2,5 @@ public class GitWrapperTester {
     public static void main(String[] args) throws Exception {
         GitWrapper git = new GitWrapper();
         git.init();
-
-        git.commit("JOE", "LOLLLLL");
-
-        // git.add("newCommittedFile");
-        // git.commit("JOE", "added newCommitedFile");
-
-        // git.checkout("d21fc267e8db63038276c484a5dc1c6102d00ab1");
     }
 }
