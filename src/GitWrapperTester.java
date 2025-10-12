a0c2802185a497600975ca8159d8d34d60daa2cd
jreiner16 <joereiner16@gmail.com>
2025-10-12 12:45:17 -0700

    Tested and got it working and recorded demo

--- src/GitWrapperTester.java
+++ src/GitWrapperTester.java
@@ -2,5 +2,11 @@ public class GitWrapperTester {
     public static void main(String[] args) throws Exception {
         GitWrapper git = new GitWrapper();
         // git.init();
+
+        // now we're going to add another file
+        // git.add("newFileToAdd");
+
+        // lastly we're going to commit these changes
+        git.commit("Joe", "Commit Message (added newFileToAdd)");
     }
 }
